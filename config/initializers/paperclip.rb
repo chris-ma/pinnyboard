b50a94bfc1ff15768ba960294352fbb8c4fb8b46
Chris Ma <crispy-studios@hotmail.com>
2015-06-06 18:53:13 +1000

    change to aws file path

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:attachment/:filename'
+Paperclip::Attachment.default_options[:path] = '/:id_partition/:filename'
